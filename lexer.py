debdc9bdd17634a417e884860a857a8cadbe490b
Erik Torres <erik.torres.aguilar@gmail.com>
2019-03-02 11:08:41 -0600

    adds tokens

--- lexer.py
+++ lexer.py
@@ -37,5 +37,28 @@ reserved = {
 }
 
 tokens = [
+	"COLON",
+	"PLUS",
+	"MINUS",
+	"MULTIPLY",
+	"DIVIDE",
+	"L_PAREN",
+	"R_PAREN",
+	"DOT",
+	"EQUAL",
+	"L_BRACKET",
+	"R_BRACKET",
 
 ] + list(reserved.values())
+
+t_COLON     = r':'
+t_PLUS      = r'\+'
+t_MINUS     = r'-'
+t_MULTIPLY  = r'\*'
+t_DIVIDE    = r'/'
+t_L_PAREN   = r'\('
+t_R_PAREN   = r'\)'
+t_DOT       = r'\.'
+t_EQUAL     = r'='
+t_L_BRACKET = r'{'
+t_R_BRACKET = r'}'
