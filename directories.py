44464962f08668a71802433ea3e74d9554b49071
Diego Astiazaran <diegoaat97@gmail.com>
2019-03-22 16:43:57 -0600

    creates getter for var type

--- directories.py
+++ directories.py
@@ -1,4 +1,5 @@
 from constants import GLOBAL_BLOCK, CLASS_BLOCK, CONSTRUCTOR_BLOCK
+import helpers
 
 # Stores variables for a block
 # Includes name, type and if it is public in case of class
@@ -10,7 +11,7 @@ class VariableDirectory:
   # Adds a variable to dictionary
   def add_variable(self, var_name, type, is_public):
     if var_name in self._variable_table:
-      raise Exception('Variable ' + var_name + ' already defined in scope.')
+      helpers.throw_error('Variable ' + var_name + ' already defined in scope.')
     self._variable_table[var_name] = [type, is_public, None]
 
   def add_dimension(self, var_name):
@@ -22,12 +23,21 @@ class VariableDirectory:
     for key, value in self._variable_table.items():
       print(key, '\t', value[0], '\t', value[1], '\t', value[2])
 
+  def get_variable_type(self, var_name):
+    if var_name in self._variable_table:
+      return self._variable_table[var_name][0]
+    else:
+      helpers.throw_error('Undeclared variable ' + var_name)
+
 class FunctionDirectory:
   # Initializes object with empty dictionary
   # Adds global block when created
   def __init__(self):
     self._function_table = {}
     self.add_block(GLOBAL_BLOCK)
+  
+  def get_entry_directory(self, block_name):
+    return self._function_table[block_name][1]
 
   # Adds a block to dictionary
   def add_block(self, block_name, type = None, is_public = None, class_name = None):
@@ -36,12 +46,12 @@ class FunctionDirectory:
       # Class block has a function directory
       if type == CLASS_BLOCK:
         if block_name in self._function_table:
-          raise Exception('Class ' + block_name + ' is already defined.')
+          helpers.throw_error('Class ' + block_name + ' is already defined.')
         directory = FunctionDirectory()
       # Other blocks have a variable directory
       else:
         if block_name in self._function_table:
-          raise Exception('Block ' + block_name + ' already defined in scope.')
+          helpers.throw_error('Block ' + block_name + ' already defined in scope.')
         directory = VariableDirectory()
       # Adds block to main function directory
       self._function_table[block_name] = [type, directory, is_public]
@@ -49,7 +59,7 @@ class FunctionDirectory:
     else:
       # Check if construcor is named as class
       if type == CONSTRUCTOR_BLOCK and block_name != class_name:
-        raise Exception('Constructor in ' + class_name + ' should be named as class.')
+        helpers.throw_error('Constructor in ' + class_name + ' should be named as class.')
       self._function_table[class_name][1].add_block(block_name, type, is_public)
 
   # Adds a variable to a VariableDirectory of a block
@@ -69,7 +79,13 @@ class FunctionDirectory:
 
   def check_class(self, class_name):
     if class_name not in self._function_table:
-      raise Exception('Class ' + class_name + ' is not defined.')
+      helpers.throw_error('Class ' + class_name + ' is not defined.')
+
+  def get_variable_type(self, var_name, block_name, class_name = None):
+    if class_name == None:
+      return self.get_entry_directory(block_name).get_variable_type(var_name)
+    else:
+      return self.get_entry_directory(class_name).get_variable_type(var_name, block_name)
 
   # Used for debugging and testing purposes
   def output(self):
