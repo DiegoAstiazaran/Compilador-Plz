c0262732d3cfb6b398bdbbdc87c21f39be064cb7
Diego Astiazaran <diegoaat97@gmail.com>
2019-04-22 17:18:32 -0500

    creates basic structure

--- CompiladorPlzApp/compiler_plz/compiler_plz/urls.py
+++ CompiladorPlzApp/compiler_plz/compiler_plz/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import *
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
