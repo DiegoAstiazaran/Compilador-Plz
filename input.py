fe0cf106e2e4bdb41aec80a51fc6d38f402caf28
Erik Torres <erik.torres.aguilar@gmail.com>
2019-04-29 15:57:54 -0500

    fixes expression with false bottoms

--- input.py
+++ input.py
@@ -1,12 +1,12 @@
 def get_input():
   try:
     # file = input('Filename: ')
-    file = "subcall"
-    file = 'tests/' + file + '_test.plz'
+    file = "erik"
+    file = 'tests/' + file + '.plz'
     with open(file, 'r') as myfile:
         s = myfile.read()
   except EOFError:
       exit()
   if not file: exit()
-  
+
   return s
